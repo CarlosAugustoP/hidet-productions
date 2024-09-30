654a971c85a4daccc361362b9488d194bf8a5f2e
CarlosAugustoP <capv2004@gmail.com>
2024-09-30 17:05:22 -0300

    change files

---
+++ hidet/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import '../app/globals.css';
+import React from 'react';
+
+export default function Index() {
+    return (
+        <div className="container">
+            <h1>Hello, World!</h1>
+        </div>
+    );
+}

29fc5de32430f91d2e40e9843874edda710a4c5f
CarlosAugustoP <capv2004@gmail.com>
2024-10-01 17:04:34 -0300

    attempting parallax

--- hidet/src/pages/index.tsx
+++ hidet/src/pages/index.tsx
@@ -1,12 +1,33 @@
 import '../app/globals.css';
-import React from 'react';
+import React, {use, useEffect} from 'react';
 import Hero from '../components/desktop/Hero';
 import WhyChoose from '../components/desktop/WhyChoose';
+
+const ParallaxSection = () => {
+    useEffect(() => {
+        const handleScroll = () => {
+            const parallaxElement: any = document.querySelector('.parallax')
+            const scrollPosition = window.scrollY;
+            parallaxElement.style.backgroundPositionY = scrollPosition * 0.2 + 'px';
+
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+    return (
+        <div className="parallax">
+            <Hero />
+        </div>
+    );
+
+}
 export default function Index() {
     return (
         <div className="h-[100vh]">
-           <Hero /> 
-           <WhyChoose />
+           <ParallaxSection/>
+           <WhyChoose   />
 
         </div>
     );

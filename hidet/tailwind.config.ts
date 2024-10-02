51be6e392273343751a46adcb00196477d428fba
CarlosAugustoP <capv2004@gmail.com>
2024-10-02 19:11:02 -0300

    refactor: aboutussection

--- hidet/tailwind.config.ts
+++ hidet/tailwind.config.ts
@@ -1,39 +1,40 @@
-import type { Config } from "tailwindcss";
+  import type { Config } from "tailwindcss";
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    debugScreens: {
-      position: ['bottom', 'left'],
-    },
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+  const config: Config = {
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+      debugScreens: {
+        position: ['bottom', 'left'],
       },
-      fontSize: {
-        'custom-md-lg': '1.125rem', 
-        'custom-3xl-4xl': '2.35rem',
-        'custom-5xl-6xl': '3.35rem',
-        'custom-xs-sm': '0.8125rem', 
-        'huge': '5.5rem'
+      extend: {
+        colors: {
+          background: "var(--background)",
+          foreground: "var(--foreground)",
+        },
+        fontSize: {
+          'custom-md-lg': '1.125rem', 
+          'custom-3xl-4xl': '2.35rem',
+          'custom-5xl-6xl': '3.35rem',
+          'custom-xs-sm': '0.8125rem', 
+          'huge': '5.5rem',
+          'xl-2xl': '1.375rem', 
+        },
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
       },
     },
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
-    },
-  },
-  plugins: [
-    require('tailwindcss-debug-screens'),
-  ],
-};
+    plugins: [
+      require('tailwindcss-debug-screens'),
+    ],
+  };
 
-export default config;
+  export default config;

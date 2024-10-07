da59663994eb7786106bdc5a3278e7a25b381f21
Gabriel Pires <gabrielpires2991@gmail.com>
2024-10-07 15:29:10 -0300

    add: postgresql and init: firebase

--- hidet/prisma/migrations/20241006221553_update_id_field/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Post" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "img" TEXT,
-    "description" TEXT,
-    "postedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
---
+++ hidet/src/pages/firebase.tsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  listAll,
+  list,
+} from "firebase/storage";
+import { storage } from "./api/firebase/firebase";
+import { v4 } from "uuid";
+
+export default function Firebase() {
+  const [imageUpload, setImageUpload] = useState<File | null>(null);
+  const [imageUrls, setImageUrls] = useState<string[]>([]);
+
+  const imagesListRef = ref(storage, "images/");
+  const uploadFile = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageUrls((prev) => [...prev, url]);
+      });
+    });
+  };
+
+  useEffect(() => {
+    listAll(imagesListRef).then((response) => {
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageUrls((prev) => [...prev, url]);
+        });
+      });
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <input
+        type="file"
+        onChange={(event) => {
+          if (event.target.files && event.target.files[0]) {
+            setImageUpload(event.target.files[0]);
+          }
+        }}
+      />
+      <button onClick={uploadFile}> Upload Image</button>
+      {imageUrls.map((url) => {
+        return <img src={url} />;
+      })}
+    </div>
+  );
+}

af7508315afe88b216e890106ce2607d08188c2a
Gabriel Pires <gabrielpires2991@gmail.com>
2024-10-23 21:12:20 -0300

    fix

--- hidet/src/components/desktop/Layout1.tsx
+++ hidet/src/components/desktop/Layout1.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 
 interface Slide {
     largeImage: {
@@ -14,19 +15,45 @@ interface Slide {
     }[];
 }
 
-const Layout1 = ({slide}: {slide: Slide}) => {
+const Layout1 = ({ slide }: { slide: Slide }) => {
     return (
         <div className='2xl:h-[600px] xl:h-[500px] lg:h-[400px] md:h-[350px] w-5/6'>
             <div className="h-full w-full bg-black border-2 border-white items-center justify-center">
-                <Image
-                    src={slide.largeImage.img}
-                    alt='Slide Large'
-                    quality={80}
-                    width={1920}
-                    height={1080}
-                    className='w-full h-full object-contain'
-                    loading='lazy'
-                />
+                <Dialog>
+                    <DialogTrigger className='w-full h-full'>
+                        <Image
+                            src={slide.largeImage.img}
+                            alt='Slide Large'
+                            quality={80}
+                            width={1920}
+                            height={1080}
+                            className='w-full h-full object-contain'
+                            loading='lazy'
+                        />
+                    </DialogTrigger>
+                    <DialogContent className=' text-white bg-black'>
+                        <DialogHeader>
+                            <DialogTitle className='flex gap-4 items-center'>
+                                <h2 className='2xl:text-4xl xl:text-2xl lg:text-xl'>{slide.largeImage.title}</h2>
+                                <p className='text-lg font-thin'>{slide.largeImage.date}</p>
+                            </DialogTitle>
+                            <DialogDescription className='mt-4 2xl:text-lg xl:text-md lg:text-sm'>
+                                This is a very long description that should wrap to the next line when it exceeds the width of the dialog.
+                            </DialogDescription>
+                            <div className='mt-6 mb-6 w-full items-center justify-center flex'>
+                                <Image
+                                    src={slide.largeImage.img}
+                                    alt='Slide Large'
+                                    quality={80}
+                                    width={1920}
+                                    height={1080}
+                                    className='w-full h-full object-contain'
+                                    loading='lazy'
+                                />
+                            </div>
+                        </DialogHeader>
+                    </DialogContent>
+                </Dialog>
             </div>
         </div>
     );
